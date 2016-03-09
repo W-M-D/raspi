fadd665a83452beb6eea67c8d5640d8865111287
Matthew Weger <matthew.d.weger@gmail.com>
2016-03-09 13:08:59 -0500

    adding change password

--- install_ERGO_sd_image.sh
+++ install_ERGO_sd_image.sh
@@ -4,16 +4,17 @@
 IMG_NAME="2015-05-05-raspbian-wheezy.img"
 CURRENT_USER=$(whoami) 
 IMG_MOUNT_PATH="/media/$CURRENT_USER/13d368bf-6dbf-4751-8ba1-88bed06bef77/"
-UNIT_ID_PATH="/etc/ERGO/unit_id"
-UNIT_ID_FILE_NAME="./current_unit_id"
-
+UNIT_ID_PATH="/etc/ERGO/UNIT_ID"
+UNIT_ID_FILE_NAME="./current_UNIT_ID"
+DEFAULT_PASSWORD=""
+UNIT_ID=""
 
 select_disks ()
 {
 sudo parted -l  2>&1  |grep "Disk /"
 
 
-read -p "What is the name of the card you want to burn to? Example: sdd :"  DISK_ID
+read -p "What is the name of the card you want to burn to? Example: sdd : "  DISK_ID
 
 DISK_INFO=$("parted -l 2>&1 |grep /dev/$DISK_ID" ) 
 
@@ -24,13 +25,37 @@ DISK_INFO=$("parted -l 2>&1 |grep /dev/$DISK_ID" )
  fi
 
 while true; do
-    read -p "Does this disk info look correct? \n $DISK_INFO \n WARNING SELECTING THE WRONG DISK CAN RESULT IN SERIOUS DAMMAGE" yn
+    read -p "Does this disk info look correct? \n $DISK_INFO \n WARNING SELECTING THE WRONG DISK CAN RESULT IN SERIOUS DAMMAGE : " yn
     case $yn in
         [Yy]* ) write_img "$DISK_ID"; break;;
         [Nn]* ) select_disks; break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+}
+
+config_password()
+{
+
+while true; do
+	read -p "Would you like to use the default DEFAULT_PASSWORD or set your own? s for set d for default. : " sd
+	case $sd in
+		[Ss]* ) set_password; break;;
+		[Dd]* ) set_default_password; break;;
+		* ) echo "S or D only.";;
+	esac
+done 
+}
+
+set_custom_password()
+{
+read -p "What would you like the password for unit #$UNIT_ID to be?" custom_password 
+set_password "$custom_password";
+
+} 
+set_password()
+{
+
 }
 
 write_img () 
@@ -57,41 +82,40 @@ if [ -z "$1" ]
 then
   return 0
 fi
-unit_id="$1"
+UNIT_ID="$1"
 UNIT_ID_PATH=$("$IMG_MOUNT_PATH$UNIT_ID_PATH")
-echo "$unit_id" |sudo tee "$UNIT_ID_PATH"
+echo "$UNIT_ID" |sudo tee "$UNIT_ID_PATH"
 
 ##change the hostname
 CURRENT_HOSTNAME=$("cat $HOSTNAME_PATH | tr -d " \t\n\r" " )
-NEW_HOSTNAME="ERGO-$unit_id"
-echo "$NEW_HOSTNAME" | sudo tee "$HOSTNAME_PATH"
-sudo sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g"  "$IMG_MOUNT_PATH/etc/hosts"
+NEW_HOSTNAME="ERGO-$UNIT_ID"
+echo "$NEW_HOSTNAME" | sudo tee "$HOSTNAME_PATH" sudo sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g"  "$IMG_MOUNT_PATH/etc/hosts"
 }
 
-select_unit_id ()
+select_UNIT_ID ()
 {
 HOSTNAME_PATH="$IMG_MOUNT_PATH/etc/hostname/"
 
-read -p "What would you like this units id to be ?" unit_id
-write_id "$unit_id"
+read -p "What would you like this units id to be ?" UNIT_ID
+write_id "$UNIT_ID"
 
 }
 
-increment_unit_id ()
+increment_UNIT_ID ()
 {
 HOSTNAME_PATH="$IMG_MOUNT_PATH/etc/hostname/"
 
 
 if [ ! -f $UNIT_ID_FILE_NAME ]; then
-  read -p "File $UNIT_ID_FILE_NAME does not exist what number would you like to start with ? " unit_id
-  echo "$unit_id" > $UNIT_ID_FILE_NAME 
+  read -p "File $UNIT_ID_FILE_NAME does not exist what number would you like to start with ? " UNIT_ID
+  echo "$UNIT_ID" > $UNIT_ID_FILE_NAME 
 fi
 
-unit_id=$(cat "$UNIT_ID_FILE_NAME") 
-write_id "$unit_id"
+UNIT_ID=$(cat "$UNIT_ID_FILE_NAME") 
+write_id "$UNIT_ID"
 
-let unit_id+=1
-echo $unit_id |sudo tee $UNIT_ID_FILE_NAME
+let UNIT_ID+=1
+echo $UNIT_ID |sudo tee $UNIT_ID_FILE_NAME
 
 }
 
@@ -102,8 +126,8 @@ echo "$UNIT_ID_PATH"
 while true; do
     read -p "Would you like to select the unit id or increment from last unit id? S for select A for increment" sa
     case $sa in
-        [Ss]* ) select_unit_id; break;;
-        [Aa]* ) increment_unit_id; break;;
+        [Ss]* ) select_UNIT_ID; break;;
+        [Aa]* ) increment_UNIT_ID; break;;
         * ) echo "Please select S or A only.";;
     esac
 done
